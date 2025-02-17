c099678bda437535e46e9d68228c1ebb379c85a5
CWS24-hub <morteza@ozlink.it>
2025-02-17 12:47:58 +0400

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -40,7 +40,7 @@ export default function ChatBox() {
     return (
         <div className="flex flex-col h-screen items-center justify-start bg-cover bg-center text-white text-center p-6"
             style={{ 
-                backgroundImage: "url('/main_cover.jpg')",
+                backgroundImage: "url('/public/main_cover.jpg')",
                 backgroundSize: "cover",
                 backgroundPosition: "center"
             }}
